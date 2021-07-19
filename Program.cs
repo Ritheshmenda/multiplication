2174fc78e4b9fb3b9e88306bc216000864c4444a
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-19 22:58:38 +0530

    [rithesh]added:multiplication

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 30;
+            int y = 50;
+            Console.WriteLine(x * y);
+
+        }
+    }
+}
